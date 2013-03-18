d16de59c00c4eba0397705ef102f262cc6fc6a7a
Oyvindkg <oyvindkg@yahoo.com>
2013-03-18 19:29:17 +0100

    Alt er bra igjen
    Laster opp en ny versjon etter at git tømte alt. Nok spam fra meg for i
    dag

--- Spillprosjekt/src/graphics/Screen.java
+++ Spillprosjekt/src/graphics/Screen.java
@@ -1,5 +1,55 @@
 package graphics;
 
-public class Screen(){
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.io.File;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Screen extends JPanel implements Runnable {
 	
+//	Threaden som kjorer gameloopen
+	public Thread thread = new Thread(this);
+	
+//	Brettet som holder et grid med blokker
+	private Board board;
+	
+//	Tar vare på rammen saa vi kan legge lyttere paa den
+	private JFrame frame;
+	
+//	Starter threaden
+	public Screen(JFrame frame) {
+		this.frame = frame;
+		setSize(frame.getSize());
+		
+		thread.start();
+	}
+	
+//	Kontrollerer alle bevegelser og timere
+	private void physics(){
+		board.physics();
+	}
+	
+//	Tegner alt som skal tegnes
+	protected void paintComponent(Graphics g) {
+//		Nullstiller bildet
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+//		Tegner brettet
+		board.draw(g);
+	}	
+
+//	Kaller metodene som kontrollerer tegning og bevegelser
+	public void run() {
+		while(true) {
+			repaint();
+			physics();
+			try {
+				Thread.sleep(2);
+			} catch (Exception e) {}
+		}
+	}
 }
--- Spillprosjekt/src/graphics/Tower.java
+++ Spillprosjekt/src/graphics/Tower.java
@@ -1,21 +1,31 @@
-package Spillprosjekt;
+package graphics;
 
+import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.ArrayList;
 
 // Maa Extende Rectangle for aa gjore det lettere aa tegne i "draw"
 public class Tower extends Rectangle{
 	
+	private ArrayList<Component> components;
 	
 //	Skyter naar timeren i "physics" kaller metoden
-	private void shoot(){}
+	private void shoot(){
+		
+	}
 	
 //	Antar du mener naar det plasseres? :) Vi maa vaere flinke til aa kommentere hvis de andre skal forstaa
-//	public Tower();
-	public void create(){}
+//	public void create(){}
+	
+	public Tower(){
+		components = new ArrayList<Component>();
+	}
 	
 //	Legge til en komponent?
-	public void addComponent(Object component){}
+	public void addComponent(Component component){
+		components.add(component);
+	}
 	
 //	Tar imot et grafikkobjekt og tegner taarnet
 	public void draw(Graphics g){}

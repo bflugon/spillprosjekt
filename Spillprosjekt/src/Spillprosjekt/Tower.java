d419a546ae65badf22d6dcfa18311c1072cd505b
Oyvindkg <oyvindkg@yahoo.com>
2013-03-18 14:59:31 +0100

    Oppdaterte Tower.java med noen kommentarer og div :)

--- Spillprosjekt/src/Spillprosjekt/Tower.java
+++ Spillprosjekt/src/Spillprosjekt/Tower.java
@@ -1,6 +1,25 @@
 package Spillprosjekt;
 
-public interface Tower {
-	public void attack();
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+// Maa Extende Rectangle for aa gjore det lettere aa tegne i "draw"
+public interface Tower extends Rectangle{
+	
+	
+//	Skyter naar timeren i "physics" kaller metoden
+	private void shoot();
+	
+//	Antar du mener naar det plasseres? :) Vi maa vaere flinke til aa kommentere hvis de andre skal forstaa
+//	public Tower();
 	public void create();
+	
+//	Legge til en komponent?
+	public void addComponent(Object component);
+	
+//	Tar imot et grafikk-objekt og tegner taarnet
+	public void draw(Graphics g);
+	
+//	Alt av timere ol skal kjores fra denne (vil kalles av gameloopen)
+	public void physics();
 }

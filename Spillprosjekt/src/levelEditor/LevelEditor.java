fccd5a4559a741b42aa776bfd173d07b2d8da85c
bflugon <bflugon@gmail.com>
2013-04-10 15:50:55 +0200

    Level Editor (nesten (ikke))

---
+++ Spillprosjekt/src/levelEditor/LevelEditor.java
@@ -0,0 +1,17 @@
+package levelEditor;
+
+public class LevelEditor {
+	int id = -1;
+	
+	public static void main() {
+		
+	}
+	
+	public void changeBlock() {
+		// setBlockID(id);
+	}
+	
+	public void changeID() {
+		//id = blokk man trykket på
+	}
+}
